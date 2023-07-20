385ab1f5fd449b2bb813c5c45c9409b9f1509d9a
ninnim <ninnim133@gmail.com>
2023-07-20 22:03:17 +0700

    changed1

--- docs/evnt.js
+++ docs/evnt.js
@@ -1,32 +1,29 @@
-// function myFunction() {
-//   document.getElementById("demo").innerHTML = "I have changed!";
-// }
 
-// function validateForm() {
-//   var name = document.getElementById("name").value;
-//   var email = document.getElementById("email").value;
-//   var subject = document.getElementById("subject").value;
-//   var message = document.getElementById("message").value;
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
 
-//   if (name == "") {
-//     alert("Please enter your name.");
-//     return false;
-//   }
+  if (name == "") {
+    alert("Please enter your name.");
+    return false;
+  }
 
-//   if (email == "") {
-//     alert("Please enter your email address.");
-//     return false;
-//   }
+  if (email == "") {
+    alert("Please enter your email address.");
+    return false;
+  }
 
-//   if (subject == "") {
-//     alert("Please enter a subject.");
-//     return false;
-//   }
+  if (subject == "") {
+    alert("Please enter a subject.");
+    return false;
+  }
 
-//   if (message == "") {
-//     alert("Please enter your message.");
-//     return false;
-//   }
+  if (message == "") {
+    alert("Please enter your message.");
+    return false;
+  }
 
-//   return true;
-// }
+  return true;
+}
