4a37a9aee1ce6c4368ebd1bc521aac76a7619fcb
ninnim <ninnim133@gmail.com>
2023-07-21 10:43:05 +0700

    changed1

--- docs/my-script.js
+++ docs/my-script.js
@@ -1,29 +1,29 @@
 
-function validateForm() {
-  var name = document.getElementById("email").value;
-  var email = document.getElementById("email").value;
-  var subject = document.getElementById("subject").value;
-  var message = document.getElementById("message").value;
+// function validateForm() {
+//   var name = document.getElementById("email").value;
+//   var email = document.getElementById("email").value;
+//   var subject = document.getElementById("subject").value;
+//   var message = document.getElementById("message").value;
 
-  if (name == "") {
-    alert("Please enter your name.");
-    return false;
-  }
+//   if (name == "") {
+//     alert("Please enter your name.");
+//     return false;
+//   }
 
-  if (email == "") {
-    alert("Please enter your email address.");
-    return false;
-  }
+//   if (email == "") {
+//     alert("Please enter your email address.");
+//     return false;
+//   }
 
-  if (subject == "") {
-    alert("Please enter a subject.");
-    return false;
-  }
+//   if (subject == "") {
+//     alert("Please enter a subject.");
+//     return false;
+//   }
 
-  if (message == "") {
-    alert("Please enter your message.");
-    return false;
-  }
+//   if (message == "") {
+//     alert("Please enter your message.");
+//     return false;
+//   }
 
-  return true;
-}
+//   return true;
+// }
